7fc9d0f5182f3cde250c9c0a160f469a23bd2c55
Rishat <virisound@gmail.com>
2022-09-11 02:09:05 +0500

    useState(), input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 // 'react' импортируем в каждый файл, где создается компонент
-import React from 'react';
+// использую хук useState
+import React, {useState} from 'react';
 
 function App() {
+//[имя переменной, имя метода (любое), которое изменяет значение переменной] = useState(значение по-умолчанию)
+  const [likes, setLikes] = useState(0);
+// МОЖНО ИСПОЛЬЗОВАТЬ НЕСКОЛЬКО ХУКОВ ПОДРЯД
+  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ'); 
+
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
   return (
     <div className="App">
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+
+      <input 
+        type="text" 
+        value={value} 
+     // onChange={event => event.target.value} // такое пишется в нативном JS
+        onChange={event => setValue(event.target.value)} // а так пишется изменяющее состояние в react
+      />
+      
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
