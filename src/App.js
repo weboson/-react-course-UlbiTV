c3b55c305cfc6d14f6c1bd1cc8e8b8e756c626bb
Rishat <virisound@gmail.com>
2022-10-07 20:19:47 +0500

    Uncontrolled default component <input> using useRef

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { useState } from 'react';
+//! 1. импортировал useRef
+import { useState, useRef } from 'react';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 
 import './styles/App.css';
 
-//! по клику по кнопке, в консоле появляется текущее значение (title) из поля input 
+// по клику по кнопке, в консоле появляется текущее значение (title) из поля input 
 function App() {
-  // состояние №1 хранит в себе массив posts для списка
+  // состояние (useState) №1 хранит в себе массив posts для списка
+  // массив постов.
   const [posts, setPosts] = useState([
+    // значение(я) по-умолчанию
     {id: 1, title: 'JavaScript 1', body: 'Description'},
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'},
   ])
   
-  //! состояние №2 хранит в себе значение по-умолчанию (''), которое в поле input 
-  const [title, setTitle] = useState('проверка');
+  // состояние №2 хранит в себе значение по-умолчанию (''), которое в поле input 
+  const [title, setTitle] = useState('');
 
-  //! обработчик события (добавить пост):
+  //! хук useRef() - которая дает ссылку на нужный элемент. Не забывать про import useRef (выше)
+  //! 2. Для этого, в нужном элементе нужно добавить атрибут ref={bodyInpuRef} 
+  //! 3. bodyInpuRef.carrent - харнит сам элемент, и если нужно можно взять атрибут bodyInpuRef.current.value
+  const bodyInputRef = useRef();
+
+  // обработчик события (добавить пост):
   const addNewPost = (e) => { // можно как стрелочную, так и декларативную  функцию (function F() {}) - не важно  
-    e.preventDefault()
-    console.log(title)
+    e.preventDefault() // сброс действия браузера на событие (отправки (кнопка отпавить или Enter) данных в форме)? которые были по-умолчанию у браузера
+    console.log(title);
+    //! берем из useref - значение атрибута value элемента input
+    console.log(bodyInputRef.current.value);
+    //! либо сам эелмент
+    console.log(bodyInputRef.current);
   }
 
   return (
@@ -34,7 +46,12 @@ function App() {
           type="text" 
           placeholder="Заголовок поста"
         />
-        <MyInput type="text" placeholder="Описание поста"/>
+        {/* НЕ УПРАВЛЯЕМЫЙ/НЕ КОНТРОЛИРУЕМЫЙ КОМПОНЕТ (с помощью useRef) */}
+        {/*! привязываем наш ref к элементу*/}
+        <input ref={bodyInputRef} type="text" />
+        {/* пока закомментировал собственный компонент MyInput, т.к. чтобы useRef() не знает куда вставлять данные, нужно ему подсказать куда, пока покажем useRef на стандартном <input>(выше)  */}
+        {/* <MyInput type="text" placeholder="Описание поста"/> */}
+
         {/* событие на кнопке */}
         <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
@@ -44,3 +61,4 @@ function App() {
 }
 
 export default App;
+//
