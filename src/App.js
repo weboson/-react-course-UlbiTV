ac1ed53a5f2e4f5748f76e66b895a0005f37b033
Rishat <virisound@gmail.com>
2022-10-13 01:23:18 +0500

    Splitting into a PostForm. Lifting State Up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-// импортировал useRef
-import { useState, useRef } from 'react';
+import {useState} from 'react';
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
-
 import './styles/App.css';
 
 function App() {
@@ -15,53 +12,15 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'},
   ])
-  
-//! Вместо нескольких состояний, ...
-  // const [title, setTitle] = useState('');
-  // const [body, setBody] = useState('');
-//! ... создадим одно, в котором будет объект со множеством ключ/значений 
-// состояние которое добавляет пост из полей <MyInput />
-const [post, getPost] = useState({ title: '', body: ''});
- 
 
-  // обработчик события (добавить пост):
-  const addNewPost = (e) => { 
-    e.preventDefault() // сброс действия браузера по умолчанию
-    //! newPost уже не нужен, ...
-    // const newPost = {
-    //   id: Date.now(),
-    //   title: post.title,
-    //   body: post.body,
-    // };
-    // добавляем к уже существующим данным (...posts) - новые (newPost)
-    //! вместо него просто ДОБАВИМ ОБЪЕКТ к старым (...post,) новые Date.now()   - остальные новые данные в MyInput (onChange)
-    setPosts([...posts, {...post, id: Date.now()}]);
-    // обнуляем (устанавливаем пустые строки) текущие значения в объекте post: title и body, 
-    // чтобы поля input были пустыми
-    getPost({ title: '', body: ''});
-  }
-
+// обработчик (для подъема состояния)
+const createPost = (newPost) => {
+  // изменяем состояние в App.js передовая через пропсы:
+  setPosts([...posts, newPost]);
+}
   return (
     <div className="App">
-      <form>
-        {/* УПРАВЛЯЕМЫЕ КОМПОНЕНТЫ */}
-        <MyInput
-          value={post.title}
-          //! это называется деструктуризация объекта: https://learn.javascript.ru/destructuring-assignment#ostatok-obekta
-          //! -------------getPost({...старые данные, новые}) 
-          onChange={(e) => getPost({...post, title: e.target.value})}
-          type="text" 
-          placeholder="Заголовок поста"
-        />
-        <MyInput
-          value={post.body}
-          onChange={(e) => getPost({...post, body: e.target.value})}
-          type="text" 
-          placeholder="Описание поста"
-        />
-        {/* событие на кнопке */}
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title="Посты про javaScript"/>
     </div>
   );
