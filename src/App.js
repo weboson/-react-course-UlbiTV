1f5d177cb2338ae8215dcc6ffaae560a95ba6c9f
Rishat <virisound@gmail.com>
2022-10-20 05:40:29 +0500

    Functional <MySelect/>

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import PostList from './components/PostList';
 import MySelect from './components/UI/select/MySelect';
 import './styles/App.css';
 
+//! SELECT
+// ! Сложноватый момент (select). Таймкод “1:08:48”: https://youtu.be/GNrdg3PzpJQ?t=4128
 function App() {
 //состояние в котором, объект с ключами / значениями, котороые ПО-УМОЛЧАНИЮ (чтобы отобразить пример списка постов)
   const [posts, setPosts] = useState([
     // значение(я) массива posts по-умолчанию
-    {id: 1, title: 'JavaScript 1', body: 'Description'},
-    {id: 2, title: 'JavaScript 2', body: 'Description'},
-    {id: 3, title: 'JavaScript 3', body: 'Description'},
+    {id: 1, title: 'A-JavaScript 1', body: 'B-Description'},
+    {id: 2, title: 'B-JavaScript 2', body: 'C-Description'},
+    {id: 3, title: 'D-JavaScript 3', body: 'A-Description'},
+    {id: 4, title: 'C-JavaScript 3', body: 'D-Description'},
   ])
 
-// обработчик (для подъема состояния)
+//! состояние для сортировки (select)
+const [selectedSort, setSelectedSort] = useState('');
+
+
+// обработчик (тема "подъем состояния")
 const createPost = (newPost) => {
   // изменяем состояние в App.js передовая через пропсы:
   setPosts([...posts, newPost]);
@@ -25,15 +32,26 @@ const removePost = (post) => {
   setPosts(posts.filter((item) => item.id !== post.id))
 }
 
+//! сортировака массива постов (MySelect)
+const sortPosts = (sort) => {
+  setSelectedSort(sort) // текущий выбор (По названию/По описанию)
+  // console.log(sort)
+  //отсортируем массив постов - воспользуемся состоянием setPosts
+  //так как на прямую мутировать состояние нельзя, сделаем копию массива  [...arr] - используя деструктуризацию
+  //чтобы отсортеровать строки используем JS-встроенный метод: https://learn.javascript.ru/array-methods#sort-fn
+  setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort]) )) 
+}
   return (
     <div className="App">
       <PostForm create={createPost}/>
       {/* style={} - это локальные стили */}
       <hr style={{margin: '15px 0'}}/> 
       <div>
-        {/*//! добавил MySelect.jsx */}
+        {/*//! MySelect.jsx */}
         <MySelect 
-          defaultValue="Cортировка:"
+          value= {selectedSort}
+          onChange={sortPosts} 
+          defaultValue="Cортировка: "
           options={[
             {value: "title", name: "По названию"}, 
             {value: "body", name: "По описанию"}
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-// в option, чтобы она была не активна, прописываем атрибут "disabled", либо disabled={true} - безразницы
+// в option, чтобы она была не активна, прописываем атрибут "disabled", либо disabled={true} - без разницы
 // также для option нужны ключи key={options.value}
-const MySelect = ({options, defaultValue}) => {
+//! onChange - обработчик
+const MySelect = ({options, defaultValue, value, onChange}) => {
     return (
-        <select>
+        <select 
+            value={value} // select.value – значение выбранного в данный момент <option>
+            onChange = {event => onChange(event.target.value)}
+        >
             <option disabled value="">{defaultValue}</option>
-            {options.map((option) => (
-                <option key={option.value} value="option.value">
+            {options.map((option) => 
+                <option key={option.value} value={option.value}>
                     {option.name}
                 </option>
-            ))}
+            )}
+            
         </select>
     );
 };
