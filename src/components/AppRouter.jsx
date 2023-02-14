e0da8a245e12bc6ab9720f53089f36515843bbf1
Rishat <virisound@gmail.com>
2023-02-14 12:12:12 +0500

    Login condition

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,32 +2,52 @@ import React from 'react';
 // import About from '../pages/About'; // страница about
 // import Posts from '../pages/Posts'; // cтраница posts
 // import Error from '../pages/Error'; // cтраница error
-//! РОУТИНГ (многостраничное приложение) 
+// import PostIdPage from '../pages/PostIdPage';
+// РОУТИНГ (многостраничное приложение) 
 import { Route, Routes } from 'react-router-dom';
-import PostIdPage from '../pages/PostIdPage';
-import { routes } from '../routes/routes'; //! объект маршрутов
+//! объекты маршрутов
+import { privateRoutes, publicRoutes } from '../routes/routes'; 
 const AppRouter = () => {
-    console.log(routes[0].element)
+    //! флажок для авторизации (true/false)
+    const isAuth = true; 
     return (
-        // ! ДИНАМИЧЕСКИЙ роутинг - то есть роутинг формируется в зависимости от данных (объекта в routes.js)
-        <Routes>
-            {routes.map((r) =>
-                <Route 
-                    path={r.path} 
-                    element={<r.element/>}
-                    exact={r.exact} 
-                    errorElement={<r.errorElement/>} />
-            )}    
-        </Routes>
+        // ДИНАМИЧЕСКИЙ роутинг - то есть роутинг формируется в зависимости от данных (объекта в routes.js)
+        //! условие авторизации 
+        isAuth
+            ?
+            <Routes>
+                {privateRoutes.map((r,i) => //! для авторизованных пользователях (isAuth ==  true)
+                    <Route
+                        key={i+2} // уникальный ключ для React - а то ошибка в консоли
+                        path={r.path}
+                        element={<r.element />}
+                        exact={r.exact}
+                        errorElement={<r.errorElement />}
+                    />
+                )}
+            </Routes>
+            :
+            <Routes>
+                {publicRoutes.map((r,i) => //! для НЕ авторизованных пользователях (isAuth ==  false)
+                    <Route
+                        key={i+3} // ключ 
+                        path={r.path}
+                        element={<r.element />}
+                        exact={r.exact}
+                        errorElement={<r.errorElement />}
+                    />
+                )}
+            </Routes>
 
 
-// статический роутинг (прошлый)
-         //<Routes>
-            // <Route
-            //    path="about" //! путь можно, как '/about', так и без слеша
-            //    element={<About />}
-            //    errorElement={<Posts />} // если компонент в About будет ошибка, то страница Posts (как работает не понял, все ошибки About вылетают в react )
-           // />
+
+        // статический роутинг (прошлый)
+        //<Routes>
+        // <Route
+        //    path="about" //! путь можно, как '/about', так и без слеша
+        //    element={<About />}
+        //    errorElement={<Posts />} // если компонент в About будет ошибка, то страница Posts (как работает не понял, все ошибки About вылетают в react )
+        // />
         //</Routes>
     );
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,16 +5,23 @@
 import About from "../pages/About";
 import Error from "../pages/Error";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 import PostIdPage from "../pages/PostIdPage";
 import Posts from "../pages/Posts";
 
-//! массив - маршруты 
-export const routes = [
+//! массив маршрутов №1: для авторизованных пользователях 
+export const privateRoutes = [ 
     {path: '/about', element: About, exact: true, errorElement: Home}, //! путь можно, как '/about', так и без слеша
     {path: '/', element: Home, exact: true, errorElement: Home}, //! и здесь главная страница
     {path: '/posts', element: Posts, exact: true, errorElement: Home}, //! exact#1 - для того, чтобы router, воспринимал указаннаые url, как РАЗНЫЕ
     {path: '/posts/:id', element: PostIdPage, exact: true, errorElement: Home},
-    {path: '/posts/:id', element: Error, exact: true, errorElement: Home},
+    {path: '*', element: Error, exact: true, errorElement: Home},
+]
+
+//! массив маршрутов №2: для не авторизованных пользователях (публичный)
+export const publicRoutes = [ 
+    {path: '/login', element: Login, exact: true, errorElement: Login}, //! страница регистрации
+    {path: '*', element: Login, exact: true, errorElement: Login}, //! перебрасывает на Login
 ]
 
 // errorElement - это если компонент в About будет ошибка, то страница Home (как работает не понял, все ошибки About вылетают в react )
