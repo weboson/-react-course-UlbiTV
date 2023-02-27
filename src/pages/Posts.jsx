93fe8c530205cf6f570813756d9d6fabbc5a55a7
Rishat <virisound@gmail.com>
2023-02-28 00:26:42 +0500

    Endless tape. Intersection Observer API

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useRef } from 'react';
 import PostFilter from '../components/PostFilter';
 import PostForm from '../components/PostForm';
 import PostList from '../components/PostList';
@@ -7,7 +7,7 @@ import MyButton from '../components/UI/button/MyButton';
 import MyModal from '../components/UI/myModal/MyModal';
 
 import { usePosts } from '../hooks/usePosts'; // КАСТОМНЫЙ ХУК
-//!import './styles/App.css';
+//import './styles/App.css';
 // хук useEffect
 import { useEffect } from 'react';
 // наш API - метод запроса списка постов с сервера
@@ -18,7 +18,7 @@ import Loader from '../components/UI/Loader/Loader';
 import { useFetching } from '../hooks/useFetching';
 // расчет страниц (аналитика) и массива парядковых чисел для кнопок
 import { getPageCount, getPagesArray } from '../utils/pages';
-//! компонент пагинации
+// компонент пагинации
 import Pagination from '../components/Pagination/Pagination';
 
 
@@ -40,11 +40,11 @@ function Posts() {
   // состояние модального окна == false
   const [modal, setModal] = useState(false);
 
-  //! состояние общего количества страниц (header: x-total-count):
+  // состояние общего количества страниц (header: x-total-count):
   const [totalPages, setTotalPages] = useState(0); //по-умолчанию ноль страниц, т.к. мы не знаем сразу скольок их
-  //! состояние параметра лимита постов (limit) 
+  // состояние параметра лимита постов (limit) 
   const [limit, setLimit] = useState(10); // - по-умолчанию 10
-  //! состояние параметра текущей страницы (page) - устанваливается в кнопках
+  // состояние параметра текущей страницы (page) - устанваливается в кнопках
   const [page, setPage] = useState(1); // - по-умолчанию первая страница (1) 
 
 
@@ -56,6 +56,11 @@ function Posts() {
   // в нашем случае, внутри модуля функция принимая все нужные пропсы, вызывает другого с частью пропсами: useSortedPosts(posts, sort)
   // используется: <PostList posts={sortedAndSearchedPosts} title="Посты про javaScript" remove={removePost}/>
 
+  //! ссылка на последний (целевой) элемент в списке постов (для бесконечной ленты):
+  const lastElement = useRef();
+  // console.log(lastElement);
+  //! useRef.current - используется для хранения Observer 
+  const observer = useRef();
 
 
 
@@ -67,19 +72,47 @@ function Posts() {
   const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
     // вставленный фрагмент из прошлого метода fetchPosts
     //  --сам axios-запрос на сервер перенесли в API--
-    //! page - получен из установленного в кнопках setPage 
+    // page - получен из установленного в кнопках setPage 
     const response = await PostService.getAll(limit, page) // обязательно await, а то posts не становиться массивом и все методы массивов выдают ошибку
     // в состояние setPosts() -  устананвливается полученный массив постов
-    setPosts(response.data) // структура данных схожа, возвращае данные это массив объектов, типа: [{id, title, body}, ...]
+    setPosts([...posts, ...response.data]) // структура данных схожа, возвращае данные это массив объектов, типа: [{id, title, body}, ...]
 
     //console.log(response.headers['x-total-count']) // скольок постов 100
-    //! получим общее количество ПОСТОВ 
+    // получим общее количество ПОСТОВ 
     const totalCount = response.headers['x-total-count']; // 100
-    //! установим новое значение состояния ОБЩЕГО КОЛИЧЕСТВА СТРАНИЦ, используя метод из pages.jsx
+    // установим новое значение состояния ОБЩЕГО КОЛИЧЕСТВА СТРАНИЦ, используя метод из pages.jsx
     //console.log(`количество страниц, если выводить порциями по 10 постов: -> ${getPageCount(response.headers['x-total-count'], limit)}`)
     setTotalPages(getPageCount(totalCount, limit)); // setTotalPages(getPageCount(100, 10)) => setTotalPages(10)
   })
 
+  //! useEffect с Observer:
+  useEffect(() => {
+    //! опции нам не нужны, ведь root по-умолчанию - видимое окно браузера, это нам и нужно
+    // var options = {
+    //   root: document.querySelector('#scrollArea'), // наблюдатель (по-умолчанию видимое окно браузера)
+    //   rootMargin: '0px', // отступы
+    //   threshold: 1.0 // процент пересечения целевого элемента
+    // }
+    //! колбэк,который запуститься (подгружает порцию постов) при пересечении границы целевого элемента
+    if (isPostsLoading) return; // если индикатор = true, то выход 
+    if (observer.current) observer.current.disconnect(); // удалять наблюдение за элементом
+    var callback = function (entries, observer) { //! entries - это массив целевых элементов (в нашем случае это только div)
+      if(entries[0].isIntersecting && page < totalPages) { //! isIntersecting - возвращает true/false в зависимости  в зоне видимости ли целевой элемент или нет
+        //console.log(entries);
+        console.log(page);
+        setPage(page + 1); //! увеличим текущую страницу, чтобы была соответствующая подгрузка постов
+        //console.log('DIV В ЗОНЕ ВИДИМОСТИ');
+      } 
+    };
+    // options нам не нужны
+    // var observer = new IntersectionObserver(callback, options);
+    //! объявление наблюдателя 
+    observer.current = new IntersectionObserver(callback); // (сохраним в useref)
+    
+    //! объявление целевого элемента
+    observer.current.observe(lastElement.current); // current это поле useRef
+    //console.log(observer.current);
+  }, [isPostsLoading]); //! зависимость 
 
   // создать пост через кнопку "создать новый пост"
   const createPost = (newPost) => {
@@ -90,15 +123,15 @@ function Posts() {
 
 
 
-  //* useEffect---------
-  // запрос на сервер происходит при первом рендере (единожды - т.к. массив пустой)
+  //* useEffect----запрос-----
+  //! запрос на сервер происходит при изменении текущей страницы (page)
   // хук useEffect - с колбеком запросов на сервер JSONPlaceholder, используя вместо fetch - библиотеку axios (в PostServise.js)
   // так как useEffect ничего не возращает (в отличии от useMemo), то обходимся без переменной
   useEffect(
     // колбэк вызывает наш метод запроса
     () => { fetchPosts(limit, page); } // метод запроса
     ,
-    [] 
+    [page] // текущая страница
   )
 
   //* удаление поста по кнопке "Удалить"
@@ -106,11 +139,10 @@ function Posts() {
     setPosts(posts.filter((item) => item.id !== post.id))
   }
 
-  //! метод для события кнопок, устанавливающий состояния: "текущая страница" [page, setPage] 
-  //! и формирование соответствующего запроса"fetchPosts" 
+  // метод для события кнопок, устанавливающий состояния: "текущая страница" [page, setPage] 
+  // и формирование соответствующего запроса"fetchPosts" 
   const changePage = (page) => {
     setPage(page); // "текущая страница" [page, setPage] (App.js)
-    fetchPosts(limit, page); // шаблон для запроса (в try...catch) из каст.хука useFetching.jsx 
   }
 
   //* логика сортировки (select) и фильтрации (поиск)
@@ -146,13 +178,16 @@ function Posts() {
       }
       {/* // индикатор загрузки (загрузки с сервера) */}
       {/* // УСЛОВИЕ индикации - если isPostsLoading == true, то индикатор появляется, если false - размонтируетсяы */}
+
+      <PostList posts={sortedAndSearchedPosts} title="Посты про javaScript" remove={removePost} />
+      <div ref={lastElement} style={{ height: 20, background: 'red' }} >Целевой элемент (Observer)</div>
       {
         isPostsLoading
-          ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>  // компонент индикатора (кружочек)
-          : <PostList posts={sortedAndSearchedPosts} title="Посты про javaScript" remove={removePost} />
+        &&
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>  // компонент индикатора (кружочек)
       }
 
-      {/*//! ПАГИНАЦИЯ  */}
+      {/*// ПАГИНАЦИЯ  */}
       <Pagination totalPages={totalPages} page={page} changePage={changePage} />
     </div>
   );
